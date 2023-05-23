a912ed09e218bad1a763534efab1229fc635a444
LMKhnh <lemykhanh20092002@gmail.com>
2023-05-23 10:11:53 +0700

    up project

---
+++ BTKetNoiFactoryIO&C#/BTKEtNoiFactoryIO&C#/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace BTKEtNoiFactoryIO_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(345, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(70, 402);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(388, 85);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(575, 402);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(388, 85);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(458, 502);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(111, 257);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(458, 130);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(111, 257);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(464, 402);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(88, 84);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(383, 516);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(47, 46);
+            this.panel6.TabIndex = 7;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(383, 568);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(47, 46);
+            this.panel7.TabIndex = 8;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(383, 620);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(47, 46);
+            this.panel8.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(956, 502);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(609, 732);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(661, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(897, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Dài";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(592, 704);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Trung Bình";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(592, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Ngắn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(301, 516);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 36);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "CB3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(301, 568);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 36);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "CB2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(301, 620);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 36);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "CB1";
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(211, 156);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(98, 100);
+            this.panel9.TabIndex = 19;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1409, 787);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BTKetNoiFactoryIO&C#/BTKEtNoiFactoryIO&C#/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using S7.Net;
+using S7.Net.Types;
+using System.Reflection.Emit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace BTKEtNoiFactoryIO_C_
+{
+    public partial class Form1 : Form
+    {
+        Plc factoryio;
+        
+        bool conveyor0, conveyor1, conveyor2, conveyor3;
+        byte[] Q = new byte[40];
+        byte[] I = new byte[4];
+        byte[] M = new byte[4];
+        int Count_C, Count_TB, Count_T;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            factoryio = new Plc(CpuType.S71500, "192.168.0.100", 0, 1);
+            factoryio.Open();
+            if (factoryio.Open() == ErrorCode.NoError)
+            {
+                M[1] |= 0x04;
+                factoryio.WriteBytes(DataType.Memory, 1, 1, M);
+                M[1] &= 0xfb;
+                factoryio.WriteBytes(DataType.Memory, 1, 1, M);
+                factoryio.Close();
+            }
+            else
+                MessageBox.Show("Can't Reset");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            factoryio = new Plc(CpuType.S71500, "192.168.0.100", 0, 1);
+            factoryio.Open();
+            if (factoryio.Open() == ErrorCode.NoError)
+            {
+                M[1] |= 0x02;
+                factoryio.WriteBytes(DataType.Memory, 1, 1, M);
+                M[1] &= 0xfd;
+                factoryio.WriteBytes(DataType.Memory, 1, 1, M);
+                
+                factoryio.Close();
+            }
+            else
+                MessageBox.Show("Can't Stop");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Start
+            factoryio = new Plc(CpuType.S71500, "192.168.0.100", 0, 1);
+            factoryio.Open();
+            if (factoryio.Open() == ErrorCode.NoError)
+            {
+                M[1] |= 0x01;
+                factoryio.WriteBytes(DataType.Memory, 1, 0, M);
+                M[1] &= 0xfe;
+                factoryio.WriteBytes(DataType.Memory, 1, 0, M);
+                factoryio.Close();
+            }
+            else
+                MessageBox.Show("Can't Start");
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            factoryio = new Plc(CpuType.S71500, "192.168.0.100", 0, 1);
+
+            if (factoryio.Open() == ErrorCode.NoError)
+            {
+                label1.Text = "Connected";
+
+                Q = factoryio.ReadBytes(DataType.Output, 0, 0, 36);
+                I = factoryio.ReadBytes(DataType.Input, 0, 0, 4);
+                
+                //Conveyor display
+
+
+                //Conveyor0
+                if (Q[0].SelectBit(0)) panel1.BackColor = Color.Green;
+                else panel1.BackColor = Color.WhiteSmoke;
+
+                //Conveyor1
+                if (Q[0].SelectBit(1)) panel2.BackColor = Color.Green;
+                else panel2.BackColor = Color.WhiteSmoke;
+
+                //Conveyor2
+                if (Q[0].SelectBit(2)) panel3.BackColor = Color.Green;
+                else panel3.BackColor = Color.WhiteSmoke;
+
+                //Conveyor3
+                if (Q[0].SelectBit(3)) panel4.BackColor = Color.Green;
+                else panel4.BackColor = Color.WhiteSmoke;
+
+                //TableTurn
+                if (Q[0].SelectBit(4)) panel5.BackColor = Color.Green;
+                else panel5.BackColor = Color.WhiteSmoke;
+
+                //Count
+                
+                Count_C = (int)Q[31];
+                Count_TB = (int)Q[33];
+                Count_T = (int)Q[35];
+                textBox1.Text = Count_C.ToString();
+                textBox2.Text = Count_TB.ToString();
+                textBox3.Text = Count_T.ToString();
+
+                //Sensor
+                if (I[0].SelectBit(3)) panel8.BackColor = Color.Blue;
+                else panel8.BackColor = Color.WhiteSmoke;
+                if (I[0].SelectBit(4)) panel7.BackColor = Color.Blue;
+                else panel7.BackColor = Color.WhiteSmoke;
+                if (I[0].SelectBit(5)) panel6.BackColor = Color.Blue;
+                else panel6.BackColor = Color.WhiteSmoke;
+
+                //Run
+                if ((bool)factoryio.Read(DataType.Memory, 0, 0, VarType.Bit, 1)) panel9.BackColor = Color.Green;
+                else panel9.BackColor = Color.Red;
+
+
+            }
+            else
+            {
+                label1.Text = "Can't connect";
+            }    
+        }
+    }
+}
